11d42f262dcad921c42802335ee88722406009ea
fabienchevalier <97484369+fabienchevalier@users.noreply.github.com>
2025-03-13 12:14:02 +0100

    fix: provider state issue

--- phase6-7/terraform/provider.tf
+++ phase6-7/terraform/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "university-terraform-state"
+    bucket = "university-example-terraform-state"
     key    = "phase6/terraform/terraform.tfstate"
     region = "us-east-1"
   }
